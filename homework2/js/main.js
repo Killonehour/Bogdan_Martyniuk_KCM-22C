49478d30c91658b6c9a2f5aab698c81f8d4f2ea4
Killonehour <80693170+Killonehour@users.noreply.github.com>
2021-06-03 17:45:17 +0300

    Add files via upload

---
+++ homework2/js/main.js
@@ -0,0 +1,14 @@
+﻿let num = prompt("Enter to number", "");
+alert("Number to enter " + num);
+
+let number = prompt("Enter to number", "");
+
+if (number % 5 == 0) {
+    for (let i = 0; i <= number; i++) {
+        if (i % 5 == 0) {
+            console.log(i);
+        }
+    }
+}
+else
+    console.log("Sorry, no numbers");
